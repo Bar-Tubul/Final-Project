902d7bd01c46ba9531c37f561bb90d297ef0202e
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-10-16 17:53:24 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -56,66 +56,52 @@ variable "db_password" {
 }
 
 # EKS Cluster Name
-# Specifies the name of the EKS cluster that will be created.
-# This name will be used across resources related to the EKS cluster.
 variable "eks_cluster_name" {
   description = "The name of the EKS cluster"
   default     = "bop-eks-cluster"
 }
 
 # Node Group Name
-# Defines the base name for the EKS worker node groups.
-# This name is used for the identification of node groups within the cluster.
 variable "node_group_name" {
   description = "The name of the node group"
   default     = "bop-node-group"
 }
 
-# AZ1 Desired Capacity
-# Specifies the desired number of worker nodes in the first availability zone (AZ1).
-# Terraform will attempt to maintain this number of nodes.
-variable "az1_desired_capacity" {
-  description = "Desired number of nodes in AZ1"
+# Application Node Group Capacity
+variable "app_desired_capacity" {
+  description = "Desired number of nodes for the application node group"
+  type        = number
   default     = 2
 }
 
-# AZ1 Maximum Size
-# Specifies the maximum number of worker nodes that can be scaled in AZ1.
-# Used for auto-scaling in case of demand spikes.
-variable "az1_max_size" {
-  description = "Maximum number of nodes in AZ1"
-  default     = 2
+variable "app_max_size" {
+  description = "Maximum number of nodes for the application node group"
+  type        = number
+  default     = 3
 }
 
-# AZ1 Minimum Size
-# Specifies the minimum number of worker nodes that should always be running in AZ1.
-# Ensures that a baseline number of nodes is available at all times.
-variable "az1_min_size" {
-  description = "Minimum number of nodes in AZ1"
+variable "app_min_size" {
+  description = "Minimum number of nodes for the application node group"
+  type        = number
   default     = 1
 }
 
-# AZ2 Desired Capacity
-# Specifies the desired number of worker nodes in the second availability zone (AZ2).
-# Like AZ1, Terraform maintains this number of nodes in AZ2.
-variable "az2_desired_capacity" {
-  description = "Desired number of nodes in AZ2"
+# Monitoring Node Group Capacity
+variable "monitoring_desired_capacity" {
+  description = "Desired number of nodes for the monitoring node group"
+  type        = number
   default     = 1
 }
 
-# AZ2 Maximum Size
-# Specifies the maximum number of worker nodes that can be scaled in AZ2.
-# Limits the number of nodes for auto-scaling during high demand.
-variable "az2_max_size" {
-  description = "Maximum number of nodes in AZ2"
+variable "monitoring_max_size" {
+  description = "Maximum number of nodes for the monitoring node group"
+  type        = number
   default     = 1
 }
 
-# AZ2 Minimum Size
-# Specifies the minimum number of worker nodes that should always be running in AZ2.
-# Ensures basic availability of resources in AZ2.
-variable "az2_min_size" {
-  description = "Minimum number of nodes in AZ2"
+variable "monitoring_min_size" {
+  description = "Minimum number of nodes for the monitoring node group"
+  type        = number
   default     = 1
 }
 
