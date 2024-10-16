ad695fab005acbeaa286298b82728b786e723988
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-10-16 19:15:10 +0300

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,18 +1,3 @@
-# Create EKS Cluster
-resource "aws_eks_cluster" "my_cluster" {
-  name     = var.eks_cluster_name
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.bop_private_subnet[0].id, 
-      aws_subnet.bop_private_subnet[1].id  # Use only two AZs
-    ]
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_policy]
-}
-
 # Create IAM Role for EKS Cluster
 resource "aws_iam_role" "eks_role" {
   name = "${var.eks_cluster_name}-role"
@@ -38,51 +23,20 @@ resource "aws_iam_role_policy_attachment" "eks_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# Node Group for Application (spans both AZs)
-resource "aws_eks_node_group" "node_group_application" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "${var.node_group_name}-application"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [
-    aws_subnet.bop_private_subnet[0].id, 
-    aws_subnet.bop_private_subnet[1].id  # Nodes in both AZs
-  ]
+# Create EKS Cluster
+resource "aws_eks_cluster" "my_cluster" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_role.arn
 
-  # Specify the security groups here
-  resources {
-    security_groups = [aws_security_group.bop_web_sg.id]  # Attach security group
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.bop_private_subnet[0].id, 
+      aws_subnet.bop_private_subnet[1].id  # Use only two AZs
+    ]
   }
 
-  scaling_config {
-    desired_size = var.app_desired_capacity
-    max_size     = var.app_max_size
-    min_size     = var.app_min_size
-  }
-
-  depends_on = [aws_eks_cluster.my_cluster]
+  depends_on = [aws_iam_role_policy_attachment.eks_policy]
 }
-
-# Node Group for Monitoring (only in one AZ)
-resource "aws_eks_node_group" "node_group_monitoring" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "${var.node_group_name}-monitoring"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.bop_private_subnet[0].id]  # Nodes only in the first AZ
-
-  # Specify the security group here as well
-  resources {
-    security_groups = [aws_security_group.bop_web_sg.id]  # Attach security group
-  }
-
-  scaling_config {
-    desired_size = var.monitoring_desired_capacity
-    max_size     = var.monitoring_max_size
-    min_size     = var.monitoring_min_size
-  }
-
-  depends_on = [aws_eks_cluster.my_cluster]
-}
-
 
 # Create IAM Role for Node Groups
 resource "aws_iam_role" "eks_node_role" {
@@ -116,4 +70,52 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
+
+# Node Group for Application (spans both AZs)
+resource "aws_eks_node_group" "node_group_application" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "${var.node_group_name}-application"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [
+    aws_subnet.bop_private_subnet[0].id, 
+    aws_subnet.bop_private_subnet[1].id  # Nodes in both AZs
+  ]
+
+  # Specify the security groups here
+  scaling_config {
+    desired_size = var.app_desired_capacity
+    max_size     = var.app_max_size
+    min_size     = var.app_min_size
+  }
+
+  # Attach security groups directly
+  resources {
+    security_groups = [aws_security_group.bop_web_sg.id]  # Attach security group
+  }
+
+  depends_on = [aws_eks_cluster.my_cluster]
+}
+
+# Node Group for Monitoring (only in one AZ)
+resource "aws_eks_node_group" "node_group_monitoring" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "${var.node_group_name}-monitoring"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.bop_private_subnet[0].id]  # Nodes only in the first AZ
+
+  # Specify the security group here as well
+  scaling_config {
+    desired_size = var.monitoring_desired_capacity
+    max_size     = var.monitoring_max_size
+    min_size     = var.monitoring_min_size
+  }
+
+  # Attach security groups directly
+  resources {
+    security_groups = [aws_security_group.bop_web_sg.id]  # Attach security group
+  }
+
+  depends_on = [aws_eks_cluster.my_cluster]
+}
+
 
