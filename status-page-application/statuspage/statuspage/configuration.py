a9757442f28680c851ef1c3e55879651cfc096ca
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-10-18 20:45:33 +0300

    Update configuration.py

--- status-page-application/statuspage/statuspage/configuration.py
+++ status-page-application/statuspage/statuspage/configuration.py
@@ -14,7 +14,7 @@ DATABASE = {
     'NAME': 'status-page',         # Database name
     'USER': 'statuspage',               # PostgreSQL username
     'PASSWORD': 'P@ssw0rd',           # PostgreSQL password
-    'HOST': '3.230.171.122',      # Database server
+    'HOST': 'bop-postgresql.cx248m4we6k7.us-east-1.rds.amazonaws.com',      # Database server
     'PORT': '5432',               # Database port (leave blank for default)
     'CONN_MAX_AGE': 300,      # Max database connection age
 }
@@ -23,7 +23,7 @@ DATABASE = {
 # for each. Full connection details are required.
 REDIS = {
     'tasks': {
-        'HOST': '3.230.171.122',
+        'HOST': 'redis',
         'PORT': 6379,
         # Comment out `HOST` and `PORT` lines and uncomment the following if using Redis Sentinel
         # 'SENTINELS': [('mysentinel.redis.example.com', 6379)],
@@ -36,7 +36,7 @@ REDIS = {
         # 'INSECURE_SKIP_TLS_VERIFY': False,
     },
     'caching': {
-        'HOST': '3.230.171.122',
+        'HOST': 'redis',
         'PORT': 6379,
         # Comment out `HOST` and `PORT` lines and uncomment the following if using Redis Sentinel
         # 'SENTINELS': [('mysentinel.redis.example.com', 6379)],
