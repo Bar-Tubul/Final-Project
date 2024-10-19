1515690755ff283f7e6809f2f5b6a7118a205bbe
Peer Vetzler <154734397+peervetzler@users.noreply.github.com>
2024-10-19 16:51:25 +0300

    Update ECR.tf

--- Terraform/ECR.tf
+++ Terraform/ECR.tf
@@ -1,11 +1,32 @@
-provider "aws" {
- 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Adjust the version as needed
+    }
+  }
+
+  required_version = ">= 1.0"
 }
 
-resource "aws_ecr_repository" "ecr_bop_repo" {
-  name                 = "bop-ecr-repo"
-  image_tag_mutability = "MUTABLE"  
-  image_scanning_configuration {
-    scan_on_push = true
+provider "aws" {
+  region = "us-east-1"  # Adjust to your desired AWS region
+}
+
+resource "aws_ecr_repository" "statuspage_bop" {
+  name                 = "statuspage-bop"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Project = "TeamC"
+  }
+}
+
+resource "aws_ecr_repository" "nginx_bop" {
+  name                 = "nginx-bop"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Project = "TeamC"
   }
 }
