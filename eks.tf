8f52919702ff128740cc3450b7ea4ddfc820488d
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-10-21 13:03:03 +0300

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,37 +1,120 @@
-# Node Group for Application (spans both AZs)
-resource "aws_eks_node_group" "node_group_application" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "${var.node_group_name}-application"  # Original name
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [
-    aws_subnet.bop_private_subnet[0].id, 
-    aws_subnet.bop_private_subnet[1].id  # Nodes in both AZs
-  ]
-  
-  # Specify instance type
-  instance_type   = "t2.medium"
+# Create IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_role" {
+  name = "${var.eks_cluster_name}-role"
 
-  scaling_config {
-    desired_size = var.app_desired_capacity
-    max_size     = var.app_max_size
-    min_size     = var.app_min_size
-  }
-
-  depends_on = [aws_eks_cluster.my_cluster]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
 }
 
-# Node Group for Application (another instance with a different name)
-resource "aws_eks_node_group" "node_group_application_1" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "${var.node_group_name}-application-1"  # New name for differentiation
+# Attach IAM Policy to EKS Cluster Role
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Create EKS Cluster
+resource "aws_eks_cluster" "bop_eks_cluster" {  # Updated cluster name reference
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.bop_private_subnet[0].id, 
+      aws_subnet.bop_private_subnet[1].id  # Use only two AZs
+    ]
+    endpoint_private_access = true  # Enable private access
+    endpoint_public_access  = false  # Disable public access for security
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_policy]
+}
+
+# Create IAM Role for Node Groups
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.node_group_name}-role"
+
+  # Add both EKS and EC2 to the trust relationship
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = [
+            "eks.amazonaws.com",  # EKS service principal
+            "ec2.amazonaws.com"   # EC2 service principal
+          ]
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach IAM Policies to Node Groups
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# Attach EC2 Container Registry read-only policy
+resource "aws_iam_role_policy_attachment" "ecr_readonly_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Create Custom ECR Policy
+resource "aws_iam_policy" "ecr_custom_policy" {
+  name        = "${var.node_group_name}-ECR-Access"
+  description = "Custom policy to allow access to ECR for pulling images"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach Custom ECR Policy to Node Groups
+resource "aws_iam_role_policy_attachment" "ecr_custom_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = aws_iam_policy.ecr_custom_policy.arn
+}
+
+# Node Group for Application (spans both AZs)
+resource "aws_eks_node_group" "node_group_application" {
+  cluster_name    = aws_eks_cluster.bop_eks_cluster.name  # Updated reference
+  node_group_name = "${var.node_group_name}-application"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = [
     aws_subnet.bop_private_subnet[0].id, 
     aws_subnet.bop_private_subnet[1].id  # Nodes in both AZs
   ]
-
-  # Specify instance type
-  instance_type   = "t2.medium"
 
   scaling_config {
     desired_size = var.app_desired_capacity
@@ -39,18 +122,15 @@ resource "aws_eks_node_group" "node_group_application_1" {
     min_size     = var.app_min_size
   }
 
-  depends_on = [aws_eks_cluster.my_cluster]
+  depends_on = [aws_eks_cluster.bop_eks_cluster]  # Updated reference
 }
 
 # Node Group for Monitoring (only in one AZ)
 resource "aws_eks_node_group" "node_group_monitoring" {
-  cluster_name    = aws_eks_cluster.my_cluster.name
+  cluster_name    = aws_eks_cluster.bop_eks_cluster.name  # Updated reference
   node_group_name = "${var.node_group_name}-monitoring"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = [aws_subnet.bop_private_subnet[0].id]  # Nodes only in the first AZ
-
-  # Specify instance type
-  instance_type   = "t2.medium"
 
   scaling_config {
     desired_size = var.monitoring_desired_capacity
@@ -58,5 +138,5 @@ resource "aws_eks_node_group" "node_group_monitoring" {
     min_size     = var.monitoring_min_size
   }
 
-  depends_on = [aws_eks_cluster.my_cluster]
+  depends_on = [aws_eks_cluster.bop_eks_cluster]  # Updated reference
 }
