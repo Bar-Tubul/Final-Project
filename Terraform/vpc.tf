a7ec76c588f9e4e0981288a20df1d26593b2eb2b
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-10-28 12:56:30 +0200

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -101,22 +101,14 @@ resource "aws_route_table_association" "bop_private_association" {
   route_table_id = aws_route_table.bop_private_rt.id
 }
 
-# Create Security Group for Web Application
-resource "aws_security_group" "bop_web_sg" {
+# Create Security Group for Status Page Application
+resource "aws_security_group" "bop_statuspage_sg" {
   vpc_id = aws_vpc.bop_vpc.id
 
   # Allow HTTP access on port 80 from anywhere
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow access on port 8080 from anywhere
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -129,6 +121,22 @@ resource "aws_security_group" "bop_web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow access for Redis on port 6379 (only from the web application security group)
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bop_statuspage_sg.id]
+  }
+
+  # Allow access for RDS on port 5432 (only from the web application security group)
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bop_statuspage_sg.id]
+  }
+
   # Allow all outbound traffic
   egress {
     from_port   = 0
@@ -138,6 +146,6 @@ resource "aws_security_group" "bop_web_sg" {
   }
 
   tags = {
-    Name = "bop-web-sg"
+    Name = "bop-statuspage-sg"
   }
 }
