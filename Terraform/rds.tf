2e4f1f3214ac2940d2f3363ecdd4b79599ff0e91
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-10-28 15:03:30 +0200

    Update rds.tf

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -1,16 +1,16 @@
 # Create Security Group for RDS Instance
 resource "aws_security_group" "bop_rds_sg" {
   name        = "bop-rds-sg"
-  description = "Allow inbound PostgreSQL traffic on port 5432 for RDS instance"
-  vpc_id      = aws_vpc.bop_vpc.id  # Reference the VPC where the RDS instance resides
+  description = "Allow access to RDS instance"
 
-  # Allow inbound traffic on PostgreSQL port 5432
+  vpc_id = aws_vpc.bop_vpc.id
+
+  # Allow inbound traffic on PostgreSQL port 5432 from EKS Nodes
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    # Replace with specific IP ranges or security groups as needed for your application
-    cidr_blocks = ["10.0.0.0/16"]  # Example VPC CIDR, tighten as needed
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eks_nodes.id]
   }
 
   # Allow all outbound traffic
@@ -26,33 +26,24 @@ resource "aws_security_group" "bop_rds_sg" {
   }
 }
 
-# Subnet Group for RDS Instance
-resource "aws_db_subnet_group" "default" {
-  name       = "bop-rds-private-subnet"  # The name of the subnet group
-  subnet_ids = aws_subnet.bop_private_subnet[*].id  # Reference to the private subnets
-
-  tags = {
-    Name = "rds-private-subnet"
-  }
-}
-
 # Create RDS Instance
-resource "aws_db_instance" "default" {
-  allocated_storage    = 20                    # Storage size in GB
-  engine               = "postgres"
-  instance_class       = "db.t3.micro"
-  username             = var.db_username
-  password             = var.db_password
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  multi_az             = true
-  vpc_security_group_ids = [aws_security_group.bop_rds_sg.id]  # Attach the security group
-  snapshot_identifier  = "updated-bop-db"
-  identifier           = "bop-statuspage"  # Set a specific identifier for the RDS instance
+resource "aws_db_instance" "bop_rds" {
+  identifier        = "bop-rds"
+  instance_class    = "db.t3.micro"
+  engine            = "postgres"
+  allocated_storage  = 20
+  username          = var.db_username
+  password          = var.db_password
+  db_name           = var.db_name
+  skip_final_snapshot = true
+
+  # Using the security group we created for RDS
+  vpc_security_group_ids = [aws_security_group.bop_rds_sg.id]
+
+  # Specify the DB subnet group if required
+  db_subnet_group_name = aws_db_subnet_group.bop_rds_subnet_group.name
 
   tags = {
-    Name = "BOP-statuspage"
+    Name = "bop-rds"
   }
-
-  depends_on = [aws_db_subnet_group.default]  # Ensure this waits for the subnet group
 }
