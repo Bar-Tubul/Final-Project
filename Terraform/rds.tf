9e322f1915fa27a39711819d32d1a4e85e834fbf
Bar-Tubul <127259698+Bar-Tubul@users.noreply.github.com>
2024-11-04 23:31:15 +0200

    Update rds.tf

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "bop_rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    security_groups = [aws_security_group.jenkins_sg.id] # Replace with actual Jenkins SG reference
+    security_groups = [aws_security_group.bop_statuspage_ec2.id] # Replace with actual Jenkins SG reference
   }
 
   # Allow all outbound traffic
